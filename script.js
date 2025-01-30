84db33f5607a82e0766445dd172c349df5dc7a4c
Ivan solano <ivanheerr13@gmail.com>
2025-01-30 16:08:27 -0500

    Mejora: Mejorandon el archivo script y haciendo boseto del diseño

--- script.js
+++ script.js
@@ -5,19 +5,6 @@ fetch('./data.json')
   .then(response => response.json())
   .then(data => {
     console.log(data);
-    // creando y renderizando los items de summary
-    data.forEach(dato => {
-      const div = document.createElement('div');
-
-      div.innerHTML = `
-      <img src="${dato.icon}" alt="icon-reaction" />
-          <p>${dato.category}</p>
-          <p>${dato.score} / 100</p>
-      `
-      summary.appendChild(div);
-      
-    });
-    
     // Calculo de la media o promedio de los scores
     let promedio = Math.round(
       data.reduce((acc, valorActual) => {
@@ -31,9 +18,46 @@ fetch('./data.json')
 
     console.log(promedio);
 
+
+    // creando y renderizando los items de summary
+    data.forEach(dato => {
+      // Creando las nuevas etiquetas o elementos
+      const div = document.createElement('div');
+      const figure = document.createElement('figure');
+      const img = document.createElement('img');
+      const figcaption = document.createElement('figcaption');
+      const p = document.createElement('p');
+      const strong = document.createElement('strong');
+
+      // Agregando los datos correspondientes a cada uno
+      img.setAttribute('src', `${dato.icon}`)
+      figcaption.textContent = `${dato.category}`
+      strong.textContent = `${dato.score}`
+
+      // Agregando img y figcaption al figure
+      figure.appendChild(img);
+      figure.appendChild(figcaption);
+
+      // Agregando el strong y el texto al p
+      p.appendChild(strong)
+      p.appendChild(document.createTextNode(' / 100'))
+
+      // Agregando el figure y el p al div
+      div.appendChild(figure);
+      div.appendChild(p);
+
+      // Agregando clases al div
+      div.classList.add('summary__score')
+
+      // Agregando el div de los score a su contenedor padre en la seccion de summary
+      summary.appendChild(div);
+
+    });
+
+
   })
   .catch(error => {
-        console.error('Error:', error);
+      console.error('Error:', error);
   });
  
 
