fc584ae78693843f639584e3bc49c9b34c7c6097
Ivan solano <ivanheerr13@gmail.com>
2025-01-30 19:35:40 -0500

    Agreando: Colores y ultimos estilos de diseño

--- script.js
+++ script.js
@@ -28,12 +28,42 @@ fetch('./data.json')
       const figcaption = document.createElement('figcaption');
       const p = document.createElement('p');
       const strong = document.createElement('strong');
-
+      
+      
       // Agregando los datos correspondientes a cada uno
-      img.setAttribute('src', `${dato.icon}`)
-      figcaption.textContent = `${dato.category}`
-      strong.textContent = `${dato.score}`
+      img.setAttribute('src', `${dato.icon}`);
+      figcaption.textContent = `${dato.category}`;
+      strong.textContent = `${dato.score}`;
+      
+      // Agregando clases
+      div.classList.add('summary__score');
+      strong.classList.add('strong--bold');
 
+      /*
+      Para agregar las clases de color de letra y fondo, hay que conparar cada figcaption 
+      para agregar sus clases correspondientes 
+      */
+      switch (figcaption.textContent) {
+        case 'Reaction':
+          figcaption.classList.add('font--red')
+          div.classList.add('background--red')
+          break;
+        case 'Memory':
+          figcaption.classList.add('font--yellow')
+          div.classList.add('background--yellow')
+          break;
+        case 'Verbal':
+          figcaption.classList.add('font--Green')
+          div.classList.add('background--Green')
+          break;
+        case 'Visual':
+          figcaption.classList.add('font--blue')
+          div.classList.add('background--blue')
+          break;
+      
+        default:
+          break;
+      }
       // Agregando img y figcaption al figure
       figure.appendChild(img);
       figure.appendChild(figcaption);
@@ -42,16 +72,14 @@ fetch('./data.json')
       p.appendChild(strong)
       p.appendChild(document.createTextNode(' / 100'))
 
+
       // Agregando el figure y el p al div
       div.appendChild(figure);
       div.appendChild(p);
 
-      // Agregando clases al div
-      div.classList.add('summary__score')
-
       // Agregando el div de los score a su contenedor padre en la seccion de summary
       summary.appendChild(div);
-
+      
     });
 
 
